6140a6a69c4ec115b57b50a908d3d7788504f6b7
Igor Resende Brito <igorbrito2705@gmail.com>
2025-12-09 00:32:20 -0300

    commit final

--- compile.bat
+++ compile.bat
@@ -1,6 +1,28 @@
 @echo off
+echo LIMPEZA COMPLETA DO PROJETO...
+
+REM Apagar classes
 del /s /q bin\*.class 2>nul
+
+REM Apagar arquivos .bin em qualquer pasta
 del /s /q *.bin 2>nul
+
+REM Apagar metadados JGroups (muito importante!!)
+del /s /q .jgroups* 2>nul
+
+REM Apagar locks
+del /s /q *.lock 2>nul
+del /s /q *.tmp 2>nul
+
+REM Apagar arquivos de dados persistidos (se existirem)
+del /s /q dados\*.bin 2>nul
+del /s /q storage\*.bin 2>nul
+
+REM Recriar pasta bin
 if not exist bin ( mkdir bin )
+
+echo RECOMPILANDO...
 javac -d bin -cp "lib\jgroups-4.2.20.Final.jar;." src\controle\*.java src\dados\*.java src\gateway\*.java src\util\*.java
+
+echo LIMPEZA E COMPILACAO FINALIZADAS.
 pause
